e1f2736f4fd39755c1f76411de373d8bc237a307
Dorai Sitaram <ds26gte@gmail.com>
2026-01-22 10:12:00 -0500

    update repo contents

---
+++ libraries-internal.poly.pm
@@ -0,0 +1,17 @@
+#lang pollen
+
+◊title[#:style '(toc)]{Internal Libraries}
+
+The following libraries are primarily meant for developers of Pyret.
+Their documentation may be poor or missing.
+You are unlikely to find much of interest here unless you're part of
+the developer team; if you are, ask us for help directly.
+
+◊(table-of-contents)
+
+◊include-section["builtin/raw-arrays.html"]
+◊include-section["trove/data-source.html"]
+◊include-section["trove/srcloc.html"]
+◊include-section["trove/pprint.html"]
+◊include-section["trove/s-exp.html"]
+◊include-section["trove/s-exp-structs.html"]
